4d57e8e8c36a44a4923e67aec0e8ceca5d429618
Hugo REY <hrey222@gmail.com>
2023-04-11 22:01:16 +0100

    better documentation

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # RPGManager-back
-appwrite databases listCollections --databaseId 642473470c82a9f670f4 --json > collections.txt
+
+## Deploy
+
+1. On your appwrite console, create a project.
+2. Create an appwrite.json file and replace project ID & project Name.
+
+## setup appwrite project with the migration-tool
+
+1. start the script and login to your appwrite account.
+2. select upload database option
+   ------ WORK IN PROGRESS ------
+3. enter the project ID and a name for the database (example: rpgmanager_db)
+4. This will create a database and all collections needed.
+5. Generate environments.ts file with the option "generate environment.ts file for frontend"
+6. You can use this file for your frontend app.
+
  
--- migration-tool.py
+++ migration-tool.py
@@ -46,6 +46,7 @@ def showMenu():
     print("1. Login")
     print("2. Get database")
     print("3. upload database")
+    print("4. generate environment.ts file for frontend")
     print("5. Exit")
     return input("Enter your choice: ")
 
@@ -62,6 +63,9 @@ if __name__ == "__main__":
             databaseid = input("Enter your database id: ")
             appwriteMigration.getDatabases(databaseid)
         elif choice == "3":
+            print("Work in progress")
             projectid = input("Enter your project id: ")
             databasename = input("Enter the database name: ")
             appwriteMigration.uploadDatabases(projectid,databasename)
+        elif choice == "4":
+            print("Work in progress")
